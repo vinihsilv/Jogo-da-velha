de931332dd0474c9d78f53bd85e3274681bfe1c2
vinihsilv <vinihsilv657@gmail.com>
2024-08-04 00:23:39 -0300

    limpando arquivos e adicionando painel principal

---
+++ jogo-da-velha/src/Components/Painel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Styles.css';
+function Painel(){
+    return(
+        <div className="bloco">
+        <a href="#" className="sobre">Sobre</a>
+        </div>
+    )
+}
+export default Painel
