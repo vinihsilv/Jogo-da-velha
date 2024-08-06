59633a1392747a191f30b227bfe2ce229e5577a4
vinihsilv <vinihsilv657@gmail.com>
2024-08-05 21:31:02 -0300

    adicionando arquitetura ITCSS

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Generic/Reset.css';
+import './Elements/Base.css'
 import Painel from './Components/Painel';
 
 function App() {
--- jogo-da-velha/src/index.js
+++ jogo-da-velha/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './Settings/Colors.css';
+import './Generic/Reset.css';
+import './Elements/Base.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
