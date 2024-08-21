a8bc959ce1d2091a423dcddcf60e8f84387c35b0
vinihsilv <vinihsilv657@gmail.com>
2024-08-20 21:49:04 -0300

    refazendo logo e sobre como objects

--- jogo-da-velha/src/Objects/Logo/index.js
+++ jogo-da-velha/src/Objects/Logo/index.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";  
 import imagem from '../../img/logo500.png';
+import './styles.css'
 function Logo (){
     return(
         <Fragment>
-            <img src={imagem} alt='logo react'/>
+            <img className="logo" src={imagem} alt='logo react'/>
         </Fragment>
     )
 }
