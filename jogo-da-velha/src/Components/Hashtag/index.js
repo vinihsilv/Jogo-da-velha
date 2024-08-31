282f154baea1b1ccc9e1327c239e3b9d7849632b
vinihsilv <vinihsilv657@gmail.com>
2024-08-30 23:25:59 -0300

    adicionando animação de seleção com css

--- jogo-da-velha/src/Components/Hashtag/index.js
+++ jogo-da-velha/src/Components/Hashtag/index.js
@@ -6,15 +6,23 @@ import Painel from '../../Components/Painel/Painel';
 function Hashtag (){
     const emptyBoard = Array(9).fill("");
     const [board, setBoard] = useState(emptyBoard);
+    const [currentPlayer, setCurrentPlayer] = useState("x");
     const handleCellClick = (index) => {
-        setBoard(board.map((item, itemIndex) => itemIndex == index ? <img src ={playero}/> : item));
-    }
+        if(board[index] !== ""){
+            return null;
+        }
+        
+        setBoard(board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item));
+
+         
+         setCurrentPlayer(currentPlayer === "x" ? "o" : "x");
+        };
     return(
         <Painel>
             <div className='board'>
                 {board.map((item, index) => (
-                    <div key={index} className={`cell $ {item}`} onClick={() => handleCellClick(index)}>
-                        {item}
+                    <div key={index} className={`cell ${item}`} onClick={() => handleCellClick(index)}>
+                        {item && <img src={item === 'x' ? playerx : playero} alt={item} />}
                     </div>
             ))}
             </div>
